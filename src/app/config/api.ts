b942d8ffa3b51636ce1c60893c6850037c91e49a
Beslin Tony Mathews <83027084+beslintony@users.noreply.github.com>
2022-03-06 23:33:53 +0100

    add api urls

---
+++ src/app/config/api.ts
@@ -0,0 +1,8 @@
+import { environment } from 'src/environments/environment';
+
+export const baseUrl = environment.production
+  ? 'https://api.mycart.com'
+  : 'http://localhost:3000';
+
+export const productsUrl = 'baseUrl' + '/products';
+export const cartsUrl = 'baseUrl' + '/carts';
