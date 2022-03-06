615da1bdbfe5ba76349826330b8af4119d04a6fe
Beslin Tony Mathews <83027084+beslintony@users.noreply.github.com>
2022-03-06 23:12:01 +0100

    update cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -15,53 +15,7 @@ type cart = {
   styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-  cartItems: cart[] = [
-    // {
-    //   id: 1,
-    //   name: 'Choc Dress Premium',
-    //   description:
-    //     'This is the best product that is available in the history of mankind. buy it now and enjoy. Limited time offer. Buy Now. Now Itself!!!!!!',
-    //   price: 120,
-    //   img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png',
-    //   quantity: 3,
-    // },
-    // {
-    //   id: 2,
-    //   name: 'Choc Pre ds',
-    //   description:
-    //     'This is the best product that is available in the history of mankind. buy it now and enjoy. Limited time offer. Buy Now. Now Itself!!!!!!',
-    //   price: 120,
-    //   img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png',
-    //   quantity: 11,
-    // },
-    // {
-    //   id: 3,
-    //   name: 'Cdfs sgsg m',
-    //   description:
-    //     'This is the best product that is available in the history of mankind. buy it now and enjoy. Limited time offer. Buy Now. Now Itself!!!!!!',
-    //   price: 15,
-    //   img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png',
-    //   quantity: 2,
-    // },
-    // {
-    //   id: 4,
-    //   name: 'Cfd f5 um',
-    //   description:
-    //     'This is the best product that is available in the history of mankind. buy it now and enjoy. Limited time offer. Buy Now. Now Itself!!!!!!',
-    //   price: 116,
-    //   img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png',
-    //   quantity: 5,
-    // },
-    // {
-    //   id: 5,
-    //   name: 'Des fo d8',
-    //   description:
-    //     'This is the best product that is available in the history of mankind. buy it now and enjoy. Limited time offer. Buy Now. Now Itself!!!!!!',
-    //   price: 60,
-    //   img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png',
-    //   quantity: 4,
-    // },
-  ];
+  cartItems: cart[] = [];
 
   cartTotal = 0;
   quantity = 1;
@@ -75,18 +29,42 @@ export class CartComponent implements OnInit {
   }
 
   addProductToCart(product: Product) {
-    this.cartItems.push({
-      id: product.id,
-      name: product.name,
-      img: product.img,
-      price: product.price,
-      quantity: this.quantity,
-    });
+    let productExists = false;
 
+    for (let item in this.cartItems) {
+      if (this.cartItems[item].id === product.id) {
+        this.cartItems[item].quantity++;
+        productExists = true;
+        break;
+      }
+    }
+    if (!productExists) {
+      this.cartItems.push({
+        id: product.id,
+        name: product.name,
+        img: product.img,
+        price: product.price,
+        quantity: this.quantity,
+      });
+    }
+
+    this.calculateCartTotal();
+  }
+
+  calculateCartTotal() {
     this.cartTotal = 0;
 
     this.cartItems?.forEach((item) => {
       this.cartTotal += item.quantity * item.price;
     });
+  }
+
+  decreaseQuantity(product: Product) {
+    for (let item in this.cartItems) {
+      if (this.cartItems[item].quantity > 1) {
+        this.cartItems[item].quantity--;
+        break;
+      }
+    }
   }
 }
