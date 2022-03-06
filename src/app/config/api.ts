7c9341aa7d70e41fef7e0a31adfd3e6d2a594f74
Beslin Tony Mathews <83027084+beslintony@users.noreply.github.com>
2022-03-06 23:37:03 +0100

    update url error

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -4,5 +4,5 @@ export const baseUrl = environment.production
   ? 'https://api.mycart.com'
   : 'http://localhost:3000';
 
-export const productsUrl = 'baseUrl' + '/products';
-export const cartsUrl = 'baseUrl' + '/carts';
+export const productsUrl = baseUrl + '/products';
+export const cartsUrl = baseUrl + '/carts';
